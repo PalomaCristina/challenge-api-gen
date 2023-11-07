a7109520e5e0d24a38d3df2f3be917f07fe91a62
palomacorrea <paloma.correa@dnkinfotelecom.com.br>
2023-11-07 07:25:44 -0300

    feat: add docker and dockercompose and prisma init with a db mongodb

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ import fastify from 'fastify';
 import 'dotenv/config'
 
 const app = fastify()
+const HOST = '0.0.0.0';
 
 var PORT = process.env.PORT
 
-app.listen({ port: PORT ? Number(PORT) : 5001 }, (err) => {
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+app.listen({ port: PORT ? Number(PORT, HOST) : 5001 }, (err) => {
     if (err) {
         console.error(err)
         process.exit(1)
